ec84c78b49bb4cde7635bf13a570829ee994b3f5
xrimir <atkamilh@gmail.com>
2021-12-24 13:25:38 +0100

    Movement logic added

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import sys
 import random
 import pygame
+import time
 
 pygame.init()
 pygame.font.init()
-size = width, height = 500, 500
+size = width, height = 1000, 1000
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Snake")
+
+
+def draw_grid(window):
+    grid_lines = 20
+    grid_width = width // grid_lines
+    x = 0
+    y = 0
+    for num in range(grid_lines):
+        x += grid_width
+        y += grid_width
+        pygame.draw.line(window, (255, 255, 255), (x, 0), (x, width))
+        pygame.draw.line(window, (255, 255, 255), (0, y), (height, y))
 
 
 class Game:
@@ -20,30 +33,30 @@ class Game:
         font = pygame.font.SysFont("Arial", 25)
         text = f"Score: {self.score}"
         x = font.render(text, False, (255, 255, 255))
-        window.blit(x, (100, 0))
-
-    def draw_grid(self, window):
-        grid_lines = 10
-        grid_width = width // grid_lines
-        x = 0
-        y = 0
-        for num in range(grid_lines):
-            x += grid_width
-            y += grid_width
-            pygame.draw.line(window, (255, 255, 255), (x, 0), (x, width))
-            pygame.draw.line(window, (255, 255, 255), (0, y), (height, y))
+        window.blit(x, (1, 1))
 
 
 class Snake:
-    x = 0
+    x = 200
     y = 0
 
     snake_width = 50
     snake_height = 50
     velocity = 50
+    direction = ""
+    snake_body = [[50, 0], [100, 0], [150, 0]]
+
+    def make_snake_bigger(self):
+        pass
+    def update_snake_body(self, window):
+        pass
+
+    def set_direction(self, direction):
+        self.direction = direction
 
     def draw_snake(self, window):
-        pygame.draw.rect(window, (0, 0, 255), [self.x, self.y, self.snake_width, self.snake_height])
+        pygame.draw.rect(window, (255, 0, 255), [self.x, self.y, self.snake_width, self.snake_height])
+
 
     def check_fruit_collision(self, fruit_x, fruit_y):
         if self.x == fruit_x and self.y == fruit_y:
@@ -56,12 +69,12 @@ class Fruit:
     fruit_width = 50
     fruit_height = 50
     fruit_color = (255, 255, 255)
-    x = random.randrange(0, 500 - fruit_width, 50)
-    y = random.randrange(0, 500 - fruit_height, 50)
+    x = random.randrange(0, width - fruit_width, 50)
+    y = random.randrange(0, height - fruit_height, 50)
 
     def refresh_fruit(self):
-        self.x = random.randrange(0, 500 - self.fruit_width, 50)
-        self.y = random.randrange(0, 500 - self.fruit_height, 50)
+        self.x = random.randrange(0, width - self.fruit_width, 50)
+        self.y = random.randrange(0, height - self.fruit_height, 50)
 
     def draw_fruit(self, window):
         pygame.draw.rect(window, self.fruit_color, [self.x, self.y, self.fruit_width, self.fruit_height])
@@ -75,21 +88,32 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w and snake.y > 0:
-                snake.y -= snake.velocity
-            if event.key == pygame.K_s and (snake.y + snake.velocity) + snake.snake_height // 2 < height:
-                snake.y += snake.velocity
-            if event.key == pygame.K_a and snake.x > 0:
-                snake.x -= snake.velocity
-            if event.key == pygame.K_d and (snake.x + snake.velocity) + snake.snake_width // 2 < width:
-                snake.x += snake.velocity
+            if event.key == pygame.K_w and snake.direction != "down":
+                snake.set_direction("up")
+            if event.key == pygame.K_s and snake.direction != "up":
+                snake.set_direction("down")
+            if event.key == pygame.K_a and snake.direction != "right":
+                snake.set_direction("left")
+            if event.key == pygame.K_d and snake.direction != "left":
+                snake.set_direction("right")
+    if snake.direction == "up" and snake.y > 0:
+        snake.y -= snake.velocity
+    elif snake.direction == "down" and (snake.y + snake.velocity) + snake.snake_height // 2 < height:
+        snake.y += snake.velocity
+    elif snake.direction == "left" and snake.x > 0:
+        snake.x -= snake.velocity
+    elif snake.direction == "right" and (snake.x + snake.velocity) + snake.snake_width // 2 < width:
+        snake.x += snake.velocity
+
     screen.fill((0, 0, 0))
-    game.draw_grid(screen)
+    draw_grid(screen)
     fruit.draw_fruit(screen)
     snake.draw_snake(screen)
+    snake.update_snake_body(screen)
     if snake.check_fruit_collision(fruit.x, fruit.y):
         fruit.refresh_fruit()
         game.score_point()
+        #snake.make_snake_bigger()
     game.render_text(screen)
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(6)
