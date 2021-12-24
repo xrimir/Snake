733c6f8719a643bd597dd727625bf51dba34af18
xrimir <atkamilh@gmail.com>
2021-12-24 15:12:01 +0100

    I have no idea what i am doing part 2 (snake_body works)

--- main.py
+++ main.py
@@ -44,19 +44,43 @@ class Snake:
     snake_height = 50
     velocity = 50
     direction = ""
-    snake_body = [[50, 0], [100, 0], [150, 0]]
+    snake_body = [{
+        "x": 200,
+        "y": 0,
+        "pos": (200, 0)
+    }]
+
+    def set_direction(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w and self.direction != "down":
+                snake.direction = "up"
+            if event.key == pygame.K_s and self.direction != "up":
+                snake.direction = "down"
+            if event.key == pygame.K_a and self.direction != "right":
+                snake.direction = "left"
+            if event.key == pygame.K_d and self.direction != "left":
+                snake.direction = "right"
 
     def make_snake_bigger(self):
-        pass
-    def update_snake_body(self, window):
-        pass
+        self.snake_body.append({
+            "x": self.snake_body[-1]["pos"][0],
+            "y": self.snake_body[-1]["pos"][1],
+            "pos": (0, 0)
+        })
 
-    def set_direction(self, direction):
-        self.direction = direction
+    def update_snake_body(self, window):
+        self.snake_body[0]["x"] = self.x
+        self.snake_body[0]["y"] = self.y
+        for num in range(1, len(self.snake_body)):
+            self.snake_body[num]["x"] = self.snake_body[num - 1]["pos"][0]
+            self.snake_body[num]["y"] = self.snake_body[num - 1]["pos"][1]
 
     def draw_snake(self, window):
         pygame.draw.rect(window, (255, 0, 255), [self.x, self.y, self.snake_width, self.snake_height])
-
+        for num in range(1, len(self.snake_body)):
+            pygame.draw.rect(window, (255, 0, 255),
+                             [self.snake_body[num]["x"], self.snake_body[num]["y"], self.snake_width,
+                              self.snake_height])
 
     def check_fruit_collision(self, fruit_x, fruit_y):
         if self.x == fruit_x and self.y == fruit_y:
@@ -80,22 +104,19 @@ class Fruit:
         pygame.draw.rect(window, self.fruit_color, [self.x, self.y, self.fruit_width, self.fruit_height])
 
 
+game = Game()
 snake = Snake()
 fruit = Fruit()
-game = Game()
 while True:
+    time.sleep(0.1)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w and snake.direction != "down":
-                snake.set_direction("up")
-            if event.key == pygame.K_s and snake.direction != "up":
-                snake.set_direction("down")
-            if event.key == pygame.K_a and snake.direction != "right":
-                snake.set_direction("left")
-            if event.key == pygame.K_d and snake.direction != "left":
-                snake.set_direction("right")
+        snake.set_direction(event)
+
+    for node in snake.snake_body:
+        node["pos"] = (node["x"], node["y"])
+
     if snake.direction == "up" and snake.y > 0:
         snake.y -= snake.velocity
     elif snake.direction == "down" and (snake.y + snake.velocity) + snake.snake_height // 2 < height:
@@ -108,12 +129,12 @@ while True:
     screen.fill((0, 0, 0))
     draw_grid(screen)
     fruit.draw_fruit(screen)
-    snake.draw_snake(screen)
     snake.update_snake_body(screen)
+    snake.draw_snake(screen)
     if snake.check_fruit_collision(fruit.x, fruit.y):
         fruit.refresh_fruit()
         game.score_point()
-        #snake.make_snake_bigger()
+        snake.make_snake_bigger()
     game.render_text(screen)
     pygame.display.update()
-    clock.tick(6)
+    clock.tick(30)
